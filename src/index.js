1db625e7049e85b8e82344bfc65e7542cbbc05b5
Dmitry Pushkarev <dmipus2016@gmail.com>
2021-05-02 23:21:39 +0300

    started twitter-like app

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-/* import App from './App';*/
+import App from './components/app/';
 import reportWebVitals from './reportWebVitals';
 
-const Header = () => <h2>Hello World!</h2>,
-      Field = () => <input type="text" placeholder="Type here"/>,
-      Btn = () => <button/>;
-
-const App = () => {
-  return (
-    <div>
-      <Header/>
-      <Field/>
-      <Btn/>
-    </div> 
-  )
-}
 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();  
