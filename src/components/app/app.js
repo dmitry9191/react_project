348d2ef742ff3906681547cf8044de85da13ef77
Dmitry Pushkarev <dmipus2016@gmail.com>
2021-05-17 22:29:36 +0300

    added react-gen-id

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,9 +4,10 @@ import SearchPanel from '../search-panel/';
 import PostStatusFilter from '../post-status-filter/';
 import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
+import nextId from 'react-id-generator';
+
 
 import './app.css';
-
 
 
 export default class App extends Component {
@@ -15,14 +16,17 @@ export default class App extends Component {
         super(props);
         this.state = {
         data: [
-                {label: "Going to React", important: true, id: 1}, 
-                {label: "That is so good..", important: false, id: 2}, 
-                {label: "I need a break!", important: false, id: 3}
+                {label: "Going to React", important: true, like: false, id: nextId()}, 
+                {label: "That is so good..", important: false, like: false, id: nextId()}, 
+                {label: "I need a break!", important: false, like: false, id: nextId()}
             ]
         }
+
+
         this.deleteItem = this.deleteItem.bind(this);
         this.addItem = this.addItem.bind(this);
-        this.maxId = 4;
+        this.onToggleImportant = this.onToggleImportant.bind(this);
+        this.onToggleLiked = this.onToggleLiked.bind(this);
     }
 
     deleteItem(id) {
@@ -40,12 +44,30 @@ export default class App extends Component {
         const newItem = {
             label: body,
             important: false,
-            id: this.maxId++
+            id: nextId()
         }
         this.setState(({data}) => {
             const newArr = [...data, newItem];
             return {
                 data: newArr
+            }
+        })
+    }
+
+    onToggleImportant(id) {
+        console.log(`important ${id}`);
+    }
+
+    onToggleLiked(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+            const old = data[index];
+            const newItem = {...old, like: !old.like}; // перезаписываем значение like которое было в old
+
+            const newArray = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+            return {
+                data: newArray
             }
         })
     }
@@ -61,6 +83,8 @@ export default class App extends Component {
                 </div>
                 <PostList posts={this.state.data}
                           onDelete={this.deleteItem}
+                          onToggleImportant={this.onToggleImportant}
+                          onToggleLiked={this.onToggleLiked}
                 />
                 <PostAddForm onAdd={this.addItem}/>
             </div>
--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -3,31 +3,8 @@ import './post-list-item.css';
 
 export default class PostListItem extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            important: false,
-            like: false
-        };
-        this.onImportant = this.onImportant.bind(this);
-        this.onLike = this.onLike.bind(this);
-    }
-
-    onImportant() {
-        this.setState(({important}) => ({
-            important: !important
-        }))
-    }
-
-    onLike() {
-        this.setState(({like}) => ({
-            like: !like
-        }))
-    }
-
     render() {
-        const {label, onDelete} = this.props;
-        const {important, like} = this.state;
+        const {label, onDelete, onToggleImportant, onToggleLiked, important, like} = this.props;
         
         let classNames = "app-list-item d-flex justify-content-between";
 
@@ -41,11 +18,11 @@ export default class PostListItem extends Component {
         
         return(
             <div className={classNames}>
-                <span onClick={this.onLike} className="app-list-item-label">
+                <span onClick={onToggleLiked} className="app-list-item-label">
                     {label}
                 </span>
                 <div className="d-flex justify-content-center align-items-center">
-                    <button onClick={this.onImportant} type="button" className="btn-star btn-sm">
+                    <button onClick={onToggleImportant} type="button" className="btn-star btn-sm">
                         <i className ="fa fa-star"></i>
                     </button>
                     <button type="button" className="btn-trash btn-sm" 
